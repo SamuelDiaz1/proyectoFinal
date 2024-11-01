b4a457032606b16b66d7d7b71e032efef032622d
SamuelDiaz1 <161249988+SamuelDiaz1@users.noreply.github.com>
2024-10-31 23:15:27 -0500

    Creacion de la clase profesor

--- tienda/src/main/java/co/edu/uniquindio/poo/Profesor.java
+++ tienda/src/main/java/co/edu/uniquindio/poo/Profesor.java
@@ -1,5 +1,33 @@
 package co.edu.uniquindio.poo;
 
-public class Profesor implements Cliente{
+public class Profesor extends Persona implements Cliente{
+    private TipoProfesor tipoProfesor;
 
+    public Profesor(String nombre, String cedula, String edad, TipoProfesor tipoProfesor) {
+        super(nombre, cedula, edad);
+        this.tipoProfesor = tipoProfesor;
+    }
+    public TipoProfesor getTipoProfesor() {
+        return tipoProfesor;
+    }
+    public void setTipoProfesor(TipoProfesor tipoProfesor) {
+        this.tipoProfesor = tipoProfesor;
+    }
+    public double calcularDescuento(){
+        double descuento;
+        switch (tipoProfesor) {
+            case SUPLENTE:
+                descuento=0.05;
+                
+            case ASOCIADO:
+                descuento=0.1;
+            case TITULAR:
+                descuento=0.15;
+            return descuento;
+            default:
+                throw new IllegalArgumentException("Tipo de profesor no valido");
+                
+        }
+        
+    }    
 }
---
+++ tienda/src/main/java/co/edu/uniquindio/poo/TipoProfesor.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.poo;
+
+public enum TipoProfesor {
+    ASOCIADO,
+    SUPLENTE,
+    TITULAR
+}
