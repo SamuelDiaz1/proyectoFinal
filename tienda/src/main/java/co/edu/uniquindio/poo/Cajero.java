05b80ba721db299856ad91f8f2409b6d9c0b01df
SamuelDiaz1 <161249988+SamuelDiaz1@users.noreply.github.com>
2024-11-05 15:53:22 -0500

    creacion clases menu, cajero y orden

---
+++ tienda/src/main/java/co/edu/uniquindio/poo/Cajero.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.poo;
+
+public class Cajero extends Empleado {
+    int horasExtra;
+    public Cajero(String nombre, String cedula, String edad, int salarioBase,int horasExtra) {
+        super(nombre, cedula, edad, salarioBase);
+        this.horasExtra=horasExtra;
+    }
+    @Override
+    public int calcularSalario() {
+        int total;
+        total=salarioBase+horasExtra;
+        return total;
+    }
+
+}
