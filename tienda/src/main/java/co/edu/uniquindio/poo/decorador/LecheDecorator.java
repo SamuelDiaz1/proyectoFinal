6d53d4bc6aab30a01de8e0413ab1b26f02b2ce0e
SamuelDiaz1 <161249988+SamuelDiaz1@users.noreply.github.com>
2024-11-07 22:14:57 -0500

    Creacion de la clase gaseosa

---
+++ tienda/src/main/java/co/edu/uniquindio/poo/decorador/LecheDecorator.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.poo.decorador;
+
+import co.edu.uniquindio.poo.Bebida;
+
+public class LecheDecorator implements Bebida{
+    private Bebida bebida;
+    public LecheDecorator(Bebida bebida) { this.bebida = bebida; }
+
+    public String getDescripcion() { return bebida.getDescripcion() + " con Leche"; }
+ 
+}
